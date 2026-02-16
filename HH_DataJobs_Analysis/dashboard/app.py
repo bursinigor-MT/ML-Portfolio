1a3e7bc95f8081f525dcb995fc9e3ea005b20b47
Igoryan <bursin04@bk.ru>
2026-02-16 14:31:58 +0300

    Added requirements.txt and updated charts

--- HH_DataJobs_Analysis/dashboard/app.py
+++ HH_DataJobs_Analysis/dashboard/app.py
@@ -1,15 +1,14 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns 
+import plotly.express as px
 import numpy as np
 import json
 
+st.set_page_config(layout="wide", page_title="HH.ru")
 
 PATH = '/home/brs/ML-portfolio/HH_DataJobs_Analysis/data/clean_data.csv'
-st.set_page_config(layout="wide")
 plt.rcParams.update({'font.size' : 16})
-sns.set_theme(palette='deep', style='whitegrid')
 
 
 @st.cache_data
@@ -29,73 +28,100 @@ def plot_DB():
     col1.metric('Общее количество вакансий: ', len(data))
     col2.metric('Общее количество работодателей: ', data['empl_name'].nunique())
     col3.metric('Общее количество необходимых навыков: ', data.explode('key_skills')['key_skills'].nunique())
-
-    st.markdown('---')
-    selected_lvl = st.selectbox('Группа', ['Intern/Junior', 'Junior/Middle', 'Middle/Senior', 'Senior/Lead'])
-    st.markdown('---')
     
-    left_col, right_col = st.columns(2)
-
-    #Скиллы
-    with left_col:
+    with st.sidebar:
+        st.title('Фильтры')
+        selected_lvl = st.selectbox('Группа', ['Intern/Junior', 'Junior/Middle', 'Middle/Senior', 'Senior/Lead', 'All'])
         selected_count_sk = st.slider('Количество навыков', 2,10,5)
+        selected_count_emp = st.slider('Количество работодателей', 2,10,5)
+
+    
+    tab_skills, tab_empl, tab_ratios = st.tabs(['Навыки', 'Работодатели', 'Обзор рынка'])
+    
+    with tab_skills: 
+        left_col, right_col = st.columns([2,1])
+
         tmp_d = data.explode('key_skills')
         rez_data = tmp_d.groupby(['level', 'key_skills']).size().reset_index(name='count')
-
-        level_data = rez_data[rez_data['level'] == selected_lvl]
+        if selected_lvl == 'All':
+            level_data = rez_data 
+        else:
+            level_data = rez_data[rez_data['level'] == selected_lvl]
         level_data = level_data.sort_values('count', ascending=False).head(selected_count_sk)
 
-        fig, ax = plt.subplots(figsize=(20, 15))
-        sns.barplot(x=level_data['key_skills'], y=level_data['count'], ax=ax)
-        ax.set_ylabel('Количество')
-        ax.set_xlabel('Навыки')
+     
 
-        st.pyplot(fig)
+        #Скиллы
+        with left_col:
+            ps_skills = px.bar(level_data, x='count', y='key_skills',
+                               color='count',
+                               color_continuous_scale='Viridis',
+                               orientation='h')
 
-    #Работодатели
-    with right_col:
-        selected_count_emp = st.slider('Количество работодателей', 2,10,5)
-        data_empl = data['empl_name'].value_counts().reset_index(name='count').sort_values('count', ascending=False).head(selected_count_emp)
+            ps_skills.update_layout(yaxis={'categoryorder':'total ascending'})
+            st.plotly_chart(ps_skills, use_container_width=True)
 
-        fig, ax = plt.subplots(figsize=(20, 15))
-        sns.barplot(x=data_empl['empl_name'], y=data_empl['count'], ax=ax)
-        ax.set_ylabel('Количество')
-        ax.set_xlabel('Работодатели')
+        with right_col:
+            tree_data = level_data.copy()
+            tree_data['parent'] = 'Все навыки'
 
-        st.pyplot(fig)
 
-    st.markdown('---')
+            ps_tree = px.treemap(tree_data, path=['parent','key_skills'], values='count',
+                                 color='count',
+                                 color_continuous_scale='Viridis',)
+            st.plotly_chart(ps_tree)
 
-    col1, col2 = st.columns(2)
+    with tab_empl:
+        left_col, right_col = st.columns([2,1]) 
 
-    with col1:
-        selected_lvl = st.selectbox('Группа', ['Intern/Junior', 'Junior/Middle', 'Middle/Senior', 'Senior/Lead', 'All'])
-        if selected_lvl == 'All':
-            data_lvl = data
-        else:
-            data_lvl = data[data['level'] == selected_lvl]
+        #Работодатели
+        with left_col:
+            data_empl = None
+            if selected_lvl == 'All':
+                level_data = data 
+            else:
+                level_data = data[data['level'] == selected_lvl]
 
-        pie_data = data_lvl['salary'].value_counts(dropna=False)
-        pie_data = pie_data.rename(index={False:'Зарплата не указана', True : 'Зарплата указана'})
-        labels = [f'{i} : {pie_data[i]}' for i in pie_data.index]
+            data_empl = level_data['empl_name'].value_counts().reset_index(name='count').sort_values('count', ascending=False).head(selected_count_emp)
 
-        fig, ax = plt.subplots(figsize=(10,10))
-        wedges, texts, autotexts = ax.pie(pie_data, labels=None, autopct='%1.1f%%')
-        ax.legend(wedges, labels, title='Категории и их количество')
+            ps_empl = px.bar(data_empl, x='count', y='empl_name',
+                             orientation='h',
+                             color='count',
+                            color_continuous_scale='Viridis')
 
-        ax.set_title(f'Доля вакансий с указанной зарплатой в группе {selected_lvl}')        
+            ps_empl.update_layout(yaxis={'categoryorder':'total ascending'})
+            st.plotly_chart(ps_empl)
 
-        st.pyplot(fig)
+        st.markdown('---')
+    
+    with tab_ratios:
+        col1, col2 = st.columns(2)
 
-    with col2:
-        selected_lvl = st.selectbox('Группа', ['All'])
-        pie_data_lvl = data['level'].value_counts()
+        with col1:
+            st.subheader('Доля указания зарплаты')
+            if selected_lvl == 'All':
+                data_lvl = data
+            else:
+                data_lvl = data[data['level'] == selected_lvl]
 
-        fig, ax = plt.subplots(figsize=(10,10))
-        ax.pie(pie_data_lvl, labels=pie_data_lvl.index, autopct='%1.1f%%')
-        ax.set_title('Доля вакансий по уровню соискателя')        
+            pie_data = data_lvl['salary'].value_counts(dropna=False)
+            pie_data = pie_data.rename(index={False:'Зарплата не указана', True : 'Зарплата указана'}).reset_index()
+            pie_data.columns = ['it_is', 'count']
 
-        st.pyplot(fig)
+            ps_salary = px.pie(pie_data, values='count', names='it_is', hole=0.4)
+            
+            st.plotly_chart(ps_salary, use_container_width=True)
+
+
+        with col2:
+            st.subheader("Распределение по грейдам")
+
+            pie_data_lvl = data['level'].value_counts().reset_index()
+            pie_data_lvl.columns = ['level', 'count']
+            ps_group_levels = px.pie(pie_data_lvl, values='count', names='level',
+                                     hole=0.4)
+                                    
+            st.plotly_chart(ps_group_levels, use_container_width=True)
 
 
 
